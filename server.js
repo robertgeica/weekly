f2fbd255f7ddddc38bf95257f270bf0aada6ad85
mr robot <geicarobert@gmail.com>
2020-07-27 16:43:45 +0300

    add register & auth routes

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 
 // Define routes
-
+app.use('/auth', require('./routes/api/auth'));
+app.use('/register', require('./routes/api/register'));
 
 // Build for heroku
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log('Server is running on port', PORT);
 });
