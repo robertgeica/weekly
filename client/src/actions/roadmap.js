53f8043c5cb3daadbdd799ded1214d750d9b4f7b
mr robot <geicarobert@gmail.com>
2020-08-08 02:20:53 +0300

    add actions and reducers for: add task, update task, delete task, task id, current task, update task modal and add task modal

--- client/src/actions/roadmap.js
+++ client/src/actions/roadmap.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 import {
 	ROADMAP_LOADED,
 	ROADMAP_ERROR,
-	CURRENT_CATEGORY_LOADED
+	CURRENT_CATEGORY,
+	ADD_TASK_MODAL,
+	ADD_TASK,
+	DELETE_TASK,
+	UPDATE_TASK,
+	UPDATE_TASK_MODAL,
+	TASK_ID,
+	CURRENT_TASK
 } from './types';
 
 // Load roadmaps from database
@@ -25,9 +32,152 @@ export const loadRoadmaps = () => async (dispatch) => {
 export const currentCategory = (category) => async (dispatch) => {
 	try {
 		dispatch({
-			type: CURRENT_CATEGORY_LOADED,
+			type: CURRENT_CATEGORY,
 			payload: category
 		});
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		});
+	}
+};
+
+// add task id to state and use it in handleUpdateTask(taskId, task) action
+export const setTaskId = id => async dispatch => {
+	try {
+		dispatch({
+			type: TASK_ID,
+			payload: id
+		})
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		})
+	}
+};
+
+// get current state to complete inputs from update modal
+export const setCurrentTask = task => async dispatch => {
+	try {
+		dispatch({
+			type: CURRENT_TASK,
+			payload: task
+		})
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		})
+	}
+}
+
+
+// MODALS actions
+// open/close add task modal
+export const handleOpenModal = () => (dispatch) => {
+	try {
+		dispatch({
+			type: ADD_TASK_MODAL,
+			payload: true
+		});
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		});
+	}
+};
+
+export const handleCloseModal = () => (dispatch) => {
+	try {
+		dispatch({
+			type: ADD_TASK_MODAL,
+			payload: false
+		});
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		});
+	}
+};
+
+// open/close update task modal
+export const handleOpenUpdateModal = () => dispatch => {
+	try {
+		dispatch({
+			type: UPDATE_TASK_MODAL,
+			payload: true
+		});
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		})
+	}
+}
+
+export const handleCloseUpdateModal = () => dispatch => {
+	try {
+		dispatch({
+			type: UPDATE_TASK_MODAL,
+			payload: false
+		});
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		})
+	}
+}
+
+// ADD, DELETE, UPDATE actions
+export const handleAddTask = (task) => async (dispatch) => {
+	try {
+		const tasks = await axios.post('http://localhost:4000/roadmap', task);
+
+		dispatch({
+			type: ADD_TASK,
+			payload: [ tasks ]
+		});
+
+		dispatch(loadRoadmaps());
+		dispatch(handleCloseModal());
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		});
+	}
+};
+
+export const handleDeleteTask = (id) => async (dispatch) => {
+	try {
+		const res = await axios.delete('http://localhost:4000/roadmap/' + id);
+
+		dispatch({
+			type: DELETE_TASK,
+			payload: res.data
+		});
+
+		dispatch(loadRoadmaps());
+	} catch (error) {
+		dispatch({
+			type: ROADMAP_ERROR
+		});
+	}
+};
+
+export const handleUpdateTask = (id, newTask) => async (dispatch) => {
+	// get task to edit
+	const res = await axios.get('http://localhost:4000/roadmap/' + id);
+	const data = res.data;
+
+	try {
+		// add new task
+		await axios.post('http://localhost:4000/roadmap/' + id, newTask);
+
+		dispatch({
+			type: UPDATE_TASK,
+			payload: [ data ]
+		});
+
+		dispatch(loadRoadmaps());
+		dispatch(handleCloseUpdateModal());
 	} catch (error) {
 		dispatch({
 			type: ROADMAP_ERROR
--- client/src/reducers/roadmap.js
+++ client/src/reducers/roadmap.js
@@ -1,8 +1,13 @@
-import { ROADMAP_LOADED, ROADMAP_ERROR, CURRENT_CATEGORY_LOADED } from '../actions/types';
+import { ROADMAP_LOADED, ROADMAP_ERROR, CURRENT_CATEGORY, ADD_TASK_MODAL, ADD_TASK, DELETE_TASK, UPDATE_TASK, UPDATE_TASK_MODAL, TASK_ID, CURRENT_TASK } from '../actions/types';
 
 const initialState = {
     data: [],
-		currentCategory: ''
+		currentCategory: '',
+		toggleAddTaskModal: false,
+		toggleUpdateTaskModal: false,
+		taskId: '',
+		currentTask: {}
+
 };
 
 export default function(state = initialState, action) {
@@ -15,11 +20,28 @@ export default function(state = initialState, action) {
 		case ROADMAP_ERROR:
 			return {...state};
 	
-		case CURRENT_CATEGORY_LOADED:
+		case CURRENT_CATEGORY:
 			return {...state, currentCategory: payload};
 
+		case ADD_TASK:
+		case UPDATE_TASK:
+		case DELETE_TASK:
+			return {...state, payload};
+		
+		case TASK_ID:
+			return { ...state, taskId: payload };
+
+		case CURRENT_TASK:
+			return { ...state, currentTask: payload };
+
+		case UPDATE_TASK_MODAL:
+			return { ...state, toggleUpdateTaskModal: payload };
+
+		case ADD_TASK_MODAL:
+			return {...state, toggleAddTaskModal: payload};
 
 		default:
 			return state;
 	}
 }
+
